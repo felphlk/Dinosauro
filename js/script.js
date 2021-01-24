bc42c24690117b9952bd7c0db6f393e1ddd3d755
felphlk <feramosvaz@hotmail.com>
2021-01-24 16:12:38 -0300

    Jogo dinossauro

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const dino= document.querySelector(".dino");
+const background=document.querySelector(".background");
+let isJumping=false
+let position=0;
+function handleKeyUp(event){
+    if(event.keyCode===32){
+        if(!isJumping){
+            jump()
+        }   
+    }
+}
+function jump(){
+    
+    isJumping=true
+    let upInterval=setInterval(()=>{
+        if(position>=150){
+            clearInterval(upInterval)
+            let downInterval=setInterval(()=>{
+                if(position<=0){
+                    clearInterval(downInterval);
+                    isJumping=false
+                }else{
+                    position-=20
+                    dino.style.bottom=position+"px"} },20)  
+        }else{
+            position+=20;
+            dino.style.bottom=position+"px"}},20)
+    
+}
+function createCactus(){
+    const cactus=document.createElement('div');
+    let cactusPosition=1000
+    let randomTime=Math.random()*1000;
+    cactus.classList.add("cactus");
+    cactus.style.left=1000+"px"
+    background.appendChild(cactus);
+    let leftInterval=setInterval(()=>{ 
+        if(cactusPosition<-60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if(cactusPosition>0 && cactusPosition<60 && position<60){
+            clearInterval(leftInterval);
+            document.body.innerHTML='<h1 class="game-over">Fim de jogo</h1>';
+       }else{
+        cactusPosition-=10;
+        cactus.style.left=cactusPosition+"px";
+        console.log(cactusPosition)
+       }
+    },20)
+    
+   setTimeout(createCactus,1000);
+}
+createCactus()
+document.addEventListener("keypress",handleKeyUp)
